f46b7f9197d8d232953104ceb3e6b86f0b6565ce
jessica288244 <jessicakim2882@gmail.com>
2025-08-27 01:50:44 +0700

    Updated responsive

--- src/pages/Feature.jsx
+++ src/pages/Feature.jsx
@@ -7,28 +7,13 @@ import { PiExportBold } from "react-icons/pi";
 import Footer from '../components/Footer';
 // import { NavLink,Navigate } from "react-router-dom";
 
-const SidebarItem = () => {
-  return (
-<div className=" text-[#12284c] rounded-lg p-6 text-center mt-6 shadow-md bg-gray-50 transition-transform">
-      <h2 className="text-3xl font-bold mb-2">Ready to Annotate Your Data?</h2>
-      <p className="mb-6 text-1xl">
-        Upload your datasets Khmer or English and get detailed validation results in seconds!
-      </p>
-      <a href="/annotate">
-      <button className="bg-[#ff3f34] text-white font-semibold px-15 py-2 rounded shadow hover:bg-white hover:text-[#ff3f34] transition">
-        Start Validate Now
-      </button>
-      </a>
 
-    </div>
-  );
-};
 
 const featuresData = [
   {
     icon: FiUpload,
     title: "Upload Dataset",
-    description: "Easily upload your image with the format (JPG/PNG).",
+    description: "Easily upload your image with format (JPG/PNG).",
     tag: "Dataset",
   },
   {
@@ -62,31 +47,51 @@ const featuresData = [
     tag: "Output",
   },
 ];
+
+const SidebarItem = () => {
+  return (
+    <div className="text-[#12284c] rounded-lg p-6 text-center mt-6 shadow-md bg-gray-50 transition-transform">
+      <h2 className="text-xl sm:text-2xl md:text-3xl font-bold mb-2">
+        Ready to Annotate Your Data?
+      </h2>
+      <p className="mb-6 text-sm sm:text-base md:text-lg">
+        Upload your datasets Khmer or English and get detailed validation results in seconds!
+      </p>
+      <a href="/annotate">
+        <button className="bg-[#ff3f34] text-white font-semibold px-4 sm:px-6 md:px-8 py-2 sm:py-2.5 md:py-3 rounded shadow hover:bg-white hover:text-[#ff3f34] transition text-xs sm:text-sm md:text-base whitespace-nowrap">
+          Start Validate Now
+        </button>
+      </a>
+    </div>
+  );
+};
+
 const Features = () => {
   return (
     <div className="min-h-full bg-gray-50">
     <main className="flex-1 p-8 bg-gray-50 overflow-y-auto">
-      <h1 className="text-5xl font-cadt text-[#ff3f34] mb-2 ">
-        Feature
+      <h1 className="text-2xl md:5xl text-center sm:text-4xl font-cadt text-[#ff3f34]">
+        Powerful Features
       </h1>
-      <p className="text-gray-600 text-lg mt-10 mb-10">
-        Everything you need to validate, clean, and improve your EN and KH datasets.
+      <p className="text-gray-600 sm:text-lg text-base mt-4 sm:mt-6 mb-4 sm:mb-6 text-center">
+        Easily upload, annotate, and validate your Khmer and English datasets with OCR extraction, precise labeling, accuracy metrics, and export options-All in one efficient tool.
       </p>
 
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4 ">
         {featuresData.map((f, i) => (
           <div
             key={i}
             div className="bg-white rounded-lg shadow p-4 flex space-x-4 
                 hover:bg-red-50 hover:shadow-lg transition 
                 duration-300 ease-in-out"
-          >
-            <div className="text-3xl text-gray-500 border-r p-6  flex border-blue-200">
+                      >
+            <div className="text-3xl text-gray-500 border-r p-6 flex justify-center items-center border-blue-200">
               <f.icon />
             </div>
+
             <div>
-              <h2 className="font-semibold text-gray-600 text-lg">{f.title}</h2>
-              <span className="inline-block px-2 py-0.5 mt-1 text-xs bg-red-100 text-gray-600 rounded">
+              <h2 className="font-semibold text-gray-600 text-base">{f.title}</h2>
+              <span className="inline-block px-2 py-0.5 mt-1 text-xs bg-red-50 text-gray-600 rounded">
                 {f.tag}
               </span>
               <p className="text-sm text-gray-600 mt-2">{f.description}</p>
